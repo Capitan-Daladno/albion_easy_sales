098046f5c11ff67be4bf110776f8e4a8d827ebe8
capitan-daladno <sodoev@gmail.com>
2020-08-02 11:38:39 +0300

    added validation before add user to event

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,12 +16,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    @user.name.downcase.titleize
 
     respond_to do |format|
       if @user.save
         format.html { redirect_to root_path, notice: 'User was successfully created.' }
       else
-        format.html { redirect_to root_path }
+        format.html { redirect_to root_path, notice: 'Какая-то ошибка, такой пользователь, возможно, уже есть.' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -52,4 +53,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :money)
     end
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -16,10 +16,27 @@ class Event < ApplicationRecord
     end
   end
 
-  def self.add_user_to_event(event, user)
-    Rails.logger.debug "KEK"
-    Rails.logger.debug user.inspect
-    event.users << user
-  end
 
+  def add_user_to_event(event, u1, user_test_name)
+    if u1 != nil && (event.users.find_by name: user_test_name) == u1
+      "already_was"
+    elsif (event.users.find_by name: u1) == nil && User.exists?(:name => user_test_name)
+      event.users << u1
+      "success_added"
+    elsif u1 == nil && user_test_name != ""
+      user = User.new
+        user.name = user_test_name.delete(" ?!:;.,()«»<>\"\-").downcase.titleize
+        if user.valid?
+          user.save!
+          event.users << user
+          "success_create_add"
+        else
+          "error_name"
+        end
+    elsif user_test_name == ""
+      "error_name"
+    else
+      "undefined_error"
+    end
+  end
 end
