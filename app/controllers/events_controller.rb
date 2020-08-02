60dfbf7d2e4c3eee38850bd7e2a2b8c673795e09
capitan-daladno <sodoev@gmail.com>
2020-08-02 13:39:48 +0300

    checkpoint for next feature

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -19,7 +19,7 @@ class EventsController < ApplicationController
     if @event.save
       redirect_to root_path, notice: 'Event was successfully created.'
     else
-      nil
+      render :new
     end
   end
 
