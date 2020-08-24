92d9516e779604dea3b2505cb630fd22f55fec82
capitan-daladno <sodoev@gmail.com>
2020-08-24 23:39:19 +0300

    delete user_id from events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_23_133510) do
+ActiveRecord::Schema.define(version: 2020_08_24_203533) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
